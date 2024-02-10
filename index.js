53569398547fb10355eaa80463d92543488bad61
ganeshdole <dole77770@gmail.com>
2024-02-10 15:06:40 +0530

    Shopping cart design completed

--- index.js
+++ index.js
@@ -2,46 +2,64 @@ import { menuArray } from "./data.js";
 const container = document.getElementById("container");
 const shoppingCart = document.getElementById("shopping-cart");
 const totalCart = document.getElementById("total-cart");
-const shoppingCartTitle = document.getElementById("title");
-
 let cartItem = [];
+
 document.addEventListener("click", (event) => {
   if (event.target.id == "add-item") {
-    renderShoppingCart(event.target.parentElement.id);
+    addItemToCart(event.target.parentElement.id);
+  } else if (event.target.id == "remove-item") {
+    console.log(event.target.parentElement.id);
+    removeItemFromCart(event.target.parentElement.id);
   }
 });
 
-function renderShoppingCart(id) {
-  shoppingCartTitle.classList.remove("hidden");
-  shoppingCart.innerHTML = addItemToCart(id);
+function addItemToCart(id) {
+  cartItem.push(menuArray[id]);
+  renderCart();
+}
+
+function renderCart() {
+  if (cartItem.length != 0) {
+    document.getElementById("title").classList.remove("hidden");
+  } else {
+    document.getElementById("title").classList.add("hidden");
+  }
+  shoppingCart.innerHTML = cartItem
+    .map((item) => {
+      return `<div id="${item.id}" class="cart">
+    <p id="cart-item" class="cart-item">${item.name}</p>
+    <button id="remove-item" class="cart-remove">remove</button>
+    <p id="cart-price" class="price">$${item.price}</p>
+  </div>`;
+    })
+    .join("");
   totalCart.innerHTML = renderTotal();
 }
 
-function addItemToCart(id) {
-  cartItem.push(menuArray[id]);
-  return render().join("");
-}
-
-function render() {
-  return cartItem.map((item) => {
-    return `<div id="${item.id}" class="cart">
-    <p id="cart-item" class="cart-item">${item.name}</p>
-    <a id="remove-item" class="cart-remove">remove</a>
-    <p id="cart-price" class="price">$${item.price}</p>
-  </div>`;
-  });
-}
-
 function renderTotal() {
-  return `
-  <hr class="width-508"/>  
+  if (cartItem.length != 0) {
+    return `<hr class="width-508"/>
   <div class="total-card">
     <p id="total-name" class="total-name">Total Price</p>
     <p class="price" id="price">$${cartItem.reduce((total, item) => {
       return total + item.price;
     }, 0)}</p>
   </div>
-  <button class="purchase-btn">Complete order</button>`;
+  <button class="purchase-btn" id="purchase-btn">Complete order</button>`;
+  }
+  return null;
+}
+
+function removeItemFromCart(id) {
+  let removed = false;
+  cartItem = cartItem.filter((item) => {
+    if (!removed && item.id === parseInt(id)) {
+      removed = true;
+      return false;
+    }
+    return true;
+  });
+  renderCart();
 }
 
 function getMenuItem(array) {
